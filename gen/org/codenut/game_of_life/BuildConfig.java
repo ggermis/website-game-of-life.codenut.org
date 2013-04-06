bab470f3bd88a7bf37fd3faa7c71e4997176d831
Gerrit Germis <gerrit.germis@up-nxt.com>
2013-04-06 15:17:22 +0200

    delete bin

--- gen/org/codenut/game_of_life/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package org.codenut.game_of_life;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- gen/org/codenut/game_of_life/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package org.codenut.game_of_life;
-
-/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
-public final class R {
-}
