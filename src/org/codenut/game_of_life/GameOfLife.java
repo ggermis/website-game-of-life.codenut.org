82082105c0b833244bf8516088bcedddbbe20e77
Gerrit Germis <gerrit.germis@up-nxt.com>
2013-04-07 22:17:39 +0200

    remove unused import

--- src/org/codenut/game_of_life/GameOfLife.java
+++ src/org/codenut/game_of_life/GameOfLife.java
@@ -2,7 +2,6 @@ package org.codenut.game_of_life;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
